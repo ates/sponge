c8f597f85eb923c8070b37597ab5860ad2d4536f
Artem Teslenko <ates@ipv6.dp.ua>
2012-10-17 10:33:48 +0200

    Add initialization of mnesia

--- apps/sponge/src/sponge_warehouse.erl
+++ apps/sponge/src/sponge_warehouse.erl
@@ -14,9 +14,27 @@
 -record(state, {}).
 
 start_link() ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+    Nodes = get_option(nodes, []),
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [Nodes], []).
 
-init([]) ->
+init([Nodes]) ->
+    lists:foreach(fun(N) -> net_kernel:connect_node(N) end, Nodes),
+    case nodes() of
+        [] ->
+            ?INFO("No nodes were found~n", []),
+            mnesia:create_schema([node()]),
+            mnesia:start(),
+            mnesia:create_table(?TABLE, [
+                {ram_copies, [node()]},
+                {attributes, record_info(fields, ?TABLE)}
+            ]);
+        N ->
+            ?INFO("Found active nodes: ~p~n", [N]),
+            mnesia:start(),
+            mnesia:change_config(extra_db_nodes, N),
+            mnesia:add_table_copy(?TABLE, node(), ram_copies),
+            mnesia:wait_for_tables([?TABLE], 30000)
+    end,
     {ok, #state{}}.
 
 handle_call(stop, _From, State) ->
@@ -29,3 +47,11 @@ handle_info(_Msg, State) ->
 
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
 terminate(_Reason, _State) -> ok.
+
+get_option(Option, Default) ->
+    case application:get_env(sponge, Option) of
+        {ok, Value} ->
+            Value;
+        undefined ->
+            Default
+    end.
