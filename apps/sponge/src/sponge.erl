73562dc337191ff2fc60d90d128fd223a78e01d6
Artem Teslenko <ates@ipv6.dp.ua>
2012-10-18 16:36:16 +0300

    Delete old code

--- apps/sponge/src/sponge.erl
+++ apps/sponge/src/sponge.erl
@@ -2,7 +2,6 @@
 
 %% API
 -export([start/0, uptime/0, debug/1]).
-%-export([get/1, set/2, set/3, incr/1, incr/2, decr/1, decr/2]).
 
 -include("sponge.hrl").
 
@@ -12,32 +11,6 @@ start() ->
     application:start(lager),
     application:start(sponge).
 
-%get(Key) ->
-%    case pg2:get_closest_pid(?GROUP) of
-%        Pid when is_pid(Pid) ->
-%            gen_server:call(Pid, {get, Key});
-%        {error, Reason} ->
-%            {error, Reason}
-%    end.
-
-%set(Key, Value) ->
-%    set(Key, Value, 0).
-%
-%set(Key, Value, TTL) ->
-%    gen_server:call(sponge_warehouse, {set, Key, Value, TTL}).
-%
-%incr(Key) ->
-%    incr(Key, 1).
-%
-%incr(Key, Incr) ->
-%    gen_server:cast(sponge_warehouse, {incr, Key, Incr}).
-%
-%decr(Key) ->
-%    decr(Key, 1).
-%
-%decr(Key, Decr) ->
-%    gen_server:cast(sponge_warehouse, {decr, Key, Decr}).
-%
 -spec uptime() -> {non_neg_integer(), calendar:time()}.
 uptime() ->
     {T, _} = erlang:statistics(wall_clock),
